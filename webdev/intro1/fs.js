6d10a6ae8beee6cf1876c8889222b3baa8a2198d
Andrei Oprea <andrei.br92@gmail.com>
2013-11-23 10:04:12 +0200

    more read file examples

--- webdev/intro1/fs.js
+++ webdev/intro1/fs.js
@@ -1,3 +1,13 @@
 var fs = require('fs')
 
-console.log(fs.readFileSync('index.js', 'utf-8'))
+//console.log(fs.readFileSync('/dev/urandom', 'utf-8'))
+
+fs.readFile('/dev/urandom', 'utf-8', function (err, file) {
+
+  console.log(file);
+
+})
+
+
+//var s = fs.createReadStream('/dev/urandom');
+//s.pipe(process.stdout);
