921757150d8ef35a4a021d058a9e66d6f306bc5e
Andrei Oprea <andrei.br92@gmail.com>
2013-12-07 10:14:04 +0200

    Add broadcast example

--- webdev/step4/index.js
+++ webdev/step4/index.js
@@ -11,6 +11,7 @@ app.get('/', function (req, res) {
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
-    console.log(data);
+  	//console.log('send to everyone else');
+    socket.broadcast.emit('broadcast', data);
   });
 });
